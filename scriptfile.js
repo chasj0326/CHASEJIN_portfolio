879a113112b41fb3dda37ee8e09a7c7193ccbb2b
chasj0326 <62418379+chasj0326@users.noreply.github.com>
2021-07-06 19:50:27 +0900

    포트폴리오 2021
    포트폴리오

---
+++ scriptfile.js
@@ -0,0 +1,124 @@
+function mover1(){
+		var T1, title1, title2, t1, t2, t;
+		title1=document.getElementById("title1");
+		title2=document.getElementById("title2");
+		t=document.getElementById("text1");
+		t1=document.getElementById("text1-1");
+		t2=document.getElementById("text1-2");
+		sub=document.getElementById("sub");
+		T1=document.getElementById("tool1");
+		
+		t.style.color="rgba(145, 162, 189, 0)";
+		t1.style.color="rgba(0, 0, 0, 1)";
+		t2.style.color="rgba(51, 114, 217, 1)";
+		title1.style.color="white";
+		title2.style.color="white";
+		T1.style.backgroundColor="rgba(101, 160, 255, 1)";
+	}
+	function mout1(){
+		var T1, title1, title2, t1, t2, t;
+		title1=document.getElementById("title1");
+		title2=document.getElementById("title2");
+		t=document.getElementById("text1");
+		t1=document.getElementById("text1-1");
+		t2=document.getElementById("text1-2");
+		T1=document.getElementById("tool1");
+		
+		t.style.color="rgba(145, 162, 189, 1)"
+		t1.style.color="rgba(0, 0, 0, 0)";
+		t2.style.color="rgba(0, 0, 0, 0)";
+		title1.style.color="#2B2A2A";
+		title2.style.color="#2B2A2A";
+		T1.style.backgroundColor="rgba(101, 160, 255, 0.35)";
+	}
+	function mover2(){
+		var T2, title3, title4, t1, t2, t;
+		title3=document.getElementById("title3");
+		title4=document.getElementById("title4");
+		t=document.getElementById("text2");
+		t1=document.getElementById("text2-1");
+		t2=document.getElementById("text2-2");
+		T2=document.getElementById("tool2");
+		
+		t.style.color="rgba(159, 122, 178, 0)";
+		t1.style.color="rgba(0, 0, 0, 1)";
+		t2.style.color="rgba(190, 72, 255, 1)";
+		title3.style.color="white";
+		title4.style.color="white";
+		T2.style.backgroundColor="rgba(209, 125, 255, 1)";
+	}
+	function mout2(){
+		var T2, title3, title4, t, t1, t2;
+		title3=document.getElementById("title3");
+		title4=document.getElementById("title4");
+		t=document.getElementById("text2");
+		t1=document.getElementById("text2-1");
+		t2=document.getElementById("text2-2");
+		T2=document.getElementById("tool2");
+		
+		t.style.color="rgba(159, 122, 178, 1)";
+		t1.style.color="rgba(0, 0, 0, 0)";
+		t2.style.color="rgba(0, 0, 0, 0)";
+		title3.style.color="#2B2A2A";
+		title4.style.color="#2B2A2A";
+		T2.style.backgroundColor="rgba(209, 125, 255, 0.35)";
+	}
+	
+	function mover3_1(){
+		var T=document.getElementById("port1");
+		T.style.border="solid 2px #9781E7";
+	}
+	function mover3_2(){
+		var T=document.getElementById("port2");
+		T.style.border="solid 2px #9781E7";
+	}
+	function mover3_3(){
+		var T=document.getElementById("port3");
+		T.style.border="solid 2px #9781E7";
+	}
+	function mover3_4(){
+		var T=document.getElementById("port4");
+		T.style.border="solid 2px #9781E7";
+	}
+	function mover3_5(){
+		var T=document.getElementById("port5");
+		T.style.border="solid 2px #9781E7";
+	}
+	function mover3_6(){
+		var T=document.getElementById("port6");
+		T.style.border="solid 2px #9781E7";
+	}
+	function mover3_7(){
+		var T=document.getElementById("port7");
+		T.style.border="solid 2px #9781E7";
+	}
+	
+	
+	function mout3_1(){
+		var T=document.getElementById("port1");
+		T.style.border="solid 2px #CCC1F3";
+	}
+	function mout3_2(){
+		var T=document.getElementById("port2");
+		T.style.border="solid 2px #CCC1F3";
+	}
+	function mout3_3(){
+		var T=document.getElementById("port3");
+		T.style.border="solid 2px #CCC1F3";
+	}
+	function mout3_4(){
+		var T=document.getElementById("port4");
+		T.style.border="solid 2px #CCC1F3";
+	}
+	function mout3_5(){
+		var T=document.getElementById("port5");
+		T.style.border="solid 2px #CCC1F3";
+	}
+	function mout3_6(){
+		var T=document.getElementById("port6");
+		T.style.border="solid 2px #CCC1F3";
+	}
+	function mout3_7(){
+		var T=document.getElementById("port7");
+		T.style.border="solid 2px #CCC1F3";
+	}
